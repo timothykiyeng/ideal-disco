b01ad1201732d1a67f11a75746c9c8e360dd2e95
Andrew-Ochieng <andrewomosh44@gmail.com>
2023-01-23 23:10:46 +0300

    add doctor's portal styles

--- src/components/PortalNav.js
+++ src/components/PortalNav.js
@@ -7,11 +7,11 @@ import { BiTestTube } from "react-icons/bi";
 
 const PortalNav = ({ user }) => {
   return (
-    <div className='portal-nav'>
+    <div className='portal-nav '>
       {user.doc ? 
-        <div className='nav-links'>
-          <NavLink to='/portal/patients'><BsFilePerson /> Patients</NavLink>
-          <NavLink to='/portal/calendar'><BsCalendarPlus /> Calendar</NavLink>
+        <div className='nav-links md:my-8 flex justify-center items-center'>
+          <NavLink className='portal-nav' to='/portal/patients'><BsFilePerson className='portal-nav-icon text-pink-500' /> Patients</NavLink>
+          <NavLink className='portal-nav' to='/portal/calendar'><BsCalendarPlus className='portal-nav-icon text-gray-700 ' /> Calendar</NavLink>
         </div>
       :
       <div className='nav-links'>
